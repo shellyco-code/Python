9364487db6b15cb47fdc95ff6ff188111e9f3026
Shelly chahar <shellychahar57@gmail.com>
2025-09-10 18:01:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+#Q1
+age= 18
+print(age)
+
+
+ #Q2
+city = "Delhi"
+print(city)
+
+
+
+#Q3
+x,y,z=100,100,100
+
+
+#non primitive data type:
+"""
+1. string : "delhi"
+2. list: [1,2,3,"delhi","hello",4.5]
+3. dictionary: {"shelly":18,"chahar":22}
+4. tuple:(a,b,c)
+5. sets: set() """
+
+
+
+#Q1: calculate the perimeter of a square (side length = 6)
+sidelength = 6
+perimeterofsquare = 4 
+print(perimeterofsquare*sidelength )
+
+#Q2: store the length and width of a rectangle and calculate area (decimals allowed)
+length_of_rectangle= 30.00
+width_Of_rectangle= 20.00
+area_of_rectangle = length_of_rectangle*width_Of_rectangle
+print(area_of_rectangle)
+
+
+#Q3: convert celsius temperature 37.5 to fahrenheit 
+temperature_in_celcius = 37.5
+temperature_in_fahrenheit = 9/5*temperature_in_celcius + 32
+print(temperature_in_fahrenheit)
+
+
+#Q4: compare the two numbers and store  thr result as a boolean.
+a,b = 100,200
+is_a_greater = (a > b) #true if a is greater than b, else false
+print(is_a_greater)
+
+
+
+
+
+
+
+
+
