15c6b6bfd0cee0dd4819f7822102fa5396462173
Shelly chahar <shellychahar57@gmail.com>
2025-09-10 18:00:03 +0530

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,12 @@
+age1=int(input("enter your age"))
+age2=int(input("enter your age"))
+s=age1+age2
+p=age1-age2
+if age1>20:
+    print('the diff of age is',p)
+elif age1<20:
+    print('the sum of age is',s)
+else:
+    print("age not valid")
+
+
