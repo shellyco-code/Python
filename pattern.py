cc2f52ebfbb5d2088f09c75f1a4be046a311d594
Shelly chahar <shellychahar57@gmail.com>
2025-09-10 18:01:14 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,16 @@
+# pattern 1
+for i in range(1,6):
+    print("*" * i)
+
+# pattern 2
+n = 5
+for i in range(6):
+    space=" " * (n - i)
+    star = "*"*i
+    
+    print(space + star)
+     
+    
+
+    
+    
